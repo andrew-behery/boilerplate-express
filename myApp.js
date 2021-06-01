104f082e89cd0cd26e9db357328337c294c2c5c4
Andrew Behery <andrewbehery@gmail.com>
2021-06-01 15:50:51 -0500

    Use the .env File, Ampt 10

--- myApp.js
+++ myApp.js
@@ -8,14 +8,13 @@ app.get("/", function(req, res) {
     res.sendFile(absolutePath)
 })
 
-app.get("/json", function(req, res){
+app.get("/json", (req, res) => {
     if(process.env.MESSAGE_STYLE === "uppercase"){
-         res.json({"message": "HELLO JSON"});
-    } else {
-         res.json({"message": "Hello json"});
+      res.json({"message" : "HELLO JSON"});
     }
-    
-})
+    res.json({"message" : "Hello json"});
+  });
+
 
 app.use("/public", express.static(__dirname + "/public"))
 
