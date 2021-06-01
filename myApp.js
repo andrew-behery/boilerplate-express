e1de3fe895c99ce58ca2708a15d19ad41fe5346f
Andrew Behery <andrewbehery@gmail.com>
2021-06-01 15:54:45 -0500

    Use the .env File, Ampt 12

--- myApp.js
+++ myApp.js
@@ -1,28 +1,18 @@
 var express = require('express');
 var app = express();
+console.log("Hello World")
 
-absolutePath = __dirname + "/views/index.html"
-
-app.get("/", function(req, res) {
-    //res.send("Hello Express")
-    res.sendFile(absolutePath)
+app.get("/", function(req, res){
+  res.sendFile(__dirname+ '/views/index.html');
+  app.use(express.static(__dirname + "/public"));
 })
-
 app.get("/json", (req, res) => {
-    if(process.env.MESSAGE_STYLE === "uppercase"){
-      res.json({"message" : "HELLO JSON"});
-    }
-    res.json({"message" : "Hello json"});
-  });
-
-
-app.use("/public", express.static(__dirname + "/public"))
-
+     if(process.env.MESSAGE_STYLE === "uppercase"){
+       res.json({"message" : "HELLO JSON"});
+     }
+     res.json({"message" : "Hello json"});
+   });
 module.exports = app;
-
-
-
-
 
 
 
