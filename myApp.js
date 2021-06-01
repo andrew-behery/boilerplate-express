17102a31a2de55fb9a408ea68a8385470ec1aa79
Andrew Behery <andrewbehery@gmail.com>
2021-06-01 14:11:26 -0500

    Serve an HTML File, Ampt 1

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+absolutePath = __dirname + "/views/index.html"
+
 app.get("/", function(req, res) {
-    res.send("Hello Express")
+    //res.send("Hello Express")
+    res.sendFile(absolutePath)
 })
 
-console.log("Hello World");
+console.log(absolutePath);
 
 
 
