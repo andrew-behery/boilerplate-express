d04df807631d5aaaa7eb2dc76ba0be1eb66a81fe
Andrew Behery <andrewbehery@gmail.com>
2021-06-01 15:48:59 -0500

    Use the .env File, Ampt 9

--- myApp.js
+++ myApp.js
@@ -9,10 +9,10 @@ app.get("/", function(req, res) {
 })
 
 app.get("/json", function(req, res){
-    if(process.env.MESSAGE_STYLE == "uppercase"){
-        return res.json({"message": "HELLO JSON"})
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+         res.json({"message": "HELLO JSON"});
     } else {
-        return res.json({"message": "Hello json"})
+         res.json({"message": "Hello json"});
     }
     
 })
