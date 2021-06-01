9bd53063365cac219c37505c5b933403ed4de4af
Andrew Behery <andrewbehery@gmail.com>
2021-06-01 16:02:29 -0500

    Use the .env File, Ampt 13

--- myApp.js
+++ myApp.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var app = express();
-console.log("Hello World")
+require('dotenv').config()
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname+ '/views/index.html');
-  app.use(express.static(__dirname + "/public"));
+absolutePath = __dirname + "/views/index.html"
+
+app.get("/", function(req, res) {
+    //res.send("Hello Express")
+    res.sendFile(absolutePath)
 })
-app.get("/json", (req, res) => {
-     if(process.env.MESSAGE_STYLE === "uppercase"){
-       res.json({"message" : "HELLO JSON"});
-     }
-     res.json({"message" : "Hello json"});
-   });
-module.exports = app;
+
+app.get("/json", function(req, res){
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+         res.json({"message": "HELLO JSON"});
+    } else {
+         res.json({"message": "Hello json"});
+    }
+    
+})
+
+app.use("/public", express.static(__dirname + "/public"))
+
+console.log(process.env.MESSAGE_STYLE)
+
+
 
 
 
