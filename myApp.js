dda7ab8f21668ba37488a69853d38fed41dbd589
Andrew Behery <andrewbehery@gmail.com>
2021-06-03 12:42:10 -0500

    Chain Middleware to Create a Time Server, Ampt 2

--- myApp.js
+++ myApp.js
@@ -60,10 +60,10 @@ app.use(express.static(__dirname + "/public"));
 
 /** 8) Chaining middleware. A Time server */
 app.get('/now', function(req, res, next) {
-  req.time = new Date().toString();  // Hypothetical synchronous operation
+  req.time = new Date().toString();
   next();
 }, function(req, res) {
-  res.send(req.time);
+  res.json({time: req.time});
 });
 
 /** 9)  Get input from client - Route parameters */
